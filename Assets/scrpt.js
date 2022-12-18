6b56ccc3f6bb17ba31534c3eb31afde3164d9bf0
John Kutch <jkutch2@yahoo.com>
2022-12-18 13:18:29 -0500

    changes to index, js for history

--- Assets/scrpt.js
+++ Assets/scrpt.js
@@ -1,27 +1,25 @@
-var formEl = document.querySelector("#city-form");
-var inputCityEl = document.querySelector("#city-name");
+var cityInputEl = document.querySelector("#city-name");
+var historyArray = [];
+var historyButtonsEl = document.querySelector("#history-buttons");
+var historyCardEl = document.querySelector("#history");
 
 var formHandler = function (event) {
     event.preventDefault();
-    var city = inputCityEl.value.trim();
-    var searchHistoryArray = []
+    var cityName = cityInputEl.value.trim();
 
-    if (city) {
-        searchHistoryArray.push(city);
-        localStorage.setItem("weatherSearch", JSON.stringify(searchHistoryArray));
-        var searchHistoryEl = document.createElement('button');
-        searchHistoryEl.className = "btn";
-        searchHistoryEl.setAttribute("data-city", city)
-        searchHistoryEl.innerHTML = city;
-        historyButtonsEl.appendChild(searchHistoryEl);
+    if (cityName) {
+        historyArray.push(cityName);
+        localStorage.setItem("weatherSearch", JSON.stringify(historyArray));
+        var historyEl = document.createElement('button');
+        historyEl.className = "btn";
+        historyEl.setAttribute("data-city", cityName)
+        historyEl.innerHTML = cityName;
+        historyButtonsEl.appendChild(historyEl);
         historyCardEl.removeAttribute("style")
-        getWeatherInfo(cityname);
-        inputCityEl.value = "";
+        getWeatherInfo(cityName);
+        cityInputEl.value = "";
     }
     else {
         alert("Please enter a City");
     }
-
 }
-
-var apiUrl ="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
